ec499c0d286f91ef85db2e44fe3c0543d20c7671
Gabriel Villanueva <97753694+Bellota22@users.noreply.github.com>
2023-02-12 10:19:19 -0500

    Finish the extraction of ulrs and titles and making a function for this

--- 3_ExtractingInfo/URL_TITLES.py
+++ 3_ExtractingInfo/URL_TITLES.py
@@ -4,6 +4,8 @@ from bs4 import BeautifulSoup
 url = 'https://www.pagina12.com.ar'
 p12 = requests.get(url)
 
+
+
 s = BeautifulSoup(p12.text, 'lxml')
 
 # List items
@@ -25,15 +27,37 @@ nav_titles = [nav[j].a.get_text() for j in range(len(nav))]
 # Doing request at the first linnk, then we need to parse again
 # because we are in another webpage
 
-sec = requests.get(nav_urls[0])
-s_sec = BeautifulSoup(sec.text, 'lxml')
+def get_links_from_section(section):
+
+    sec = requests.get(section)
+    s_sec = BeautifulSoup(sec.text, 'lxml')
 
 
-art_principal = s_sec.find('div', attrs= {'class':'article-item__content' }).find('a')
-art_principal_url = url+art_principal.get('href')
-# print (art_principal_url)
+    art_principal = s_sec.find('div', attrs = {'class':'article-item__content' }).find('a')
+    art_principal_url = section + art_principal.get('href')
+    # print (art_principal)
 
-art_secondary = s_sec.find_all('h3')
-print(art_secondary)
-# art_secondary_url= url+art_secondary.get('href')
-# print(art_secondary_url)
+    art_secondary = s_sec.find_all('h3', attrs = {'class':'h2'})
+    art_secondary_urls = [ section + art_secondary[i].a.get('href') for i in range(len(art_secondary))] 
+    # print(art_secondary_urls)
+
+    art_terciary = s_sec.find_all('h4', attrs = {'class':'h2'})
+    art_terciary_urls = [ section + art_terciary[i].a.get('href') for i in range(len(art_terciary))]
+
+    # print(art_terciary_urls)
+
+    urls_el_pais =[ art_principal_url , art_secondary_urls , art_terciary_urls]
+
+    return urls_el_pais
+
+# print(get_links_from_section(nav_urls[2]))
+
+
+urls_all_section = []
+
+for i in range(3):
+
+    links = get_links_from_section(nav_urls[i])
+    urls_all_section.append(links)
+
+print(urls_all_section)
